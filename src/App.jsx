4699fdbbf6f6631ba8b766653d62d9480a38990d
IFEANYI ANYANWU <anyanwuifeanyi001@gmail.com>
2023-01-24 13:42:25 +0100

    initialization

---
+++ src/App.jsx
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals";
+import Cart from "./components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+    const [cartIsShown, setCartIsShown] = useState(false);
+
+    const hideCartHandler = () => {
+        setCartIsShown(false);
+    };
+    const showCartHandler = () => {
+        setCartIsShown(true);
+    };
+    return (
+        <CartProvider>
+            {cartIsShown && <Cart onHideCart={hideCartHandler} />}
+            <Header onShowCart={showCartHandler} />
+            <main>
+                <Meals />
+            </main>
+        </CartProvider>
+    );
+}
+
+export default App;
